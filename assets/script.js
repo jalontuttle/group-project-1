a492f28621b6e1d2a2b4a4bf68345eb2db3228c7
Jalon Tuttle <jalontuttle@gmail.com>
2021-10-19 14:21:01 -0400

    first url working and getting info, updates to html

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,19 @@ var flightNum = $("#flight-search");
 function getFlightMap(event){
     event.preventDefault()
     console.log(flightNum.val());
-    var url = "http://flightxml.flightaware.com/json/FlightXML2/MapFlight?q=" + flightNum.val()
+    var requestUrl = "http://flightxml.flightaware.com/json/FlightXML2/MapFlight?ident=" + flightNum.val()
+
+    $.ajax({
+    url: requestUrl,
+    method: "GET",
+    dataType: "jsonp",
+    jsonp: "jsonp_callback",
+    }).then(function (response){
+        console.log(response);
+        var image = $("#image");
+        image.attr("src", "data:image/png;base64," + response.MapFlightResult);
+    })
 }
-
-
 
 
 
